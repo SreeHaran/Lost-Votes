3acf050c50ff983b81c8c67460d84ad1184ce994
subash sakthivel <subashsakthivel13122001@gmail.com>
2020-10-28 05:58:19 -0700

    finshed UI

---
+++ myvotewebapp/script.js
@@ -0,0 +1,6 @@
+
+if(un ==="admin"){
+    <a href="firstpage.html"><button>sumbit</button></a>
+}else{
+    console.log("poda");
+}
---
+++ myvotewebapp/votepage.php
@@ -0,0 +1,10 @@
+<?php
+$user = $_POST['un'];
+$pass = $_POST['pwd'];
+if($user == "admin" && $pass =='12345'){
+    
+}else
+{
+    
+}
+?>
